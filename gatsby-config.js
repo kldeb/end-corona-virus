06d78a811c7a6ed295e72052b0c96c0bfe0221a8
kldeb <karam.debly@gmail.com>
2020-03-06 22:27:28 -0500

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  siteMetadata: {
+    title: `EndCoronaVirus`,
+    description: `End Corona EndCoronaVirus`,
+    author: `@end_COVID19`,
+  },
+  plugins: [
+    'gatsby-plugin-eslint',
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-tailwind`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#4dc0b5`,
+        display: `minimal-ui`,
+        icon: `src/images/tailwind-icon.png`,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-postcss',
+      options: {
+        postCssPlugins: [
+          require(`tailwindcss`)(`./tailwind.config.js`),
+          require(`autoprefixer`),
+          require(`cssnano`),
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        tailwind: true,
+        purgeOnly: [`src/css/style.css`],
+      },
+    },
+    `gatsby-plugin-offline`,
+  ],
+};
