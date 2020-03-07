2778704a12a8ec83791548a7bc2d6ec95a242970
kldeb <karam.debly@gmail.com>
2020-03-06 23:58:33 -0500

    [wip] buttons

--- src/pages/index.js
+++ src/pages/index.js
@@ -129,6 +129,13 @@ function IndexPage() {
             <P>In public or shared spaces</P>
           </Box>
         </div>
+        <p className="my-12">You can easily share these instructions with...</p>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded uppercase mr-10">
+          Share instruction
+        </button>
+        <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded uppercase">
+          Download
+        </button>
       </section>
     </Layout>
   );
